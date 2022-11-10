8d301a726cc5f0fdd8086c09829a895c3c6b0aae
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-11-10 13:25:23 -0600

    fix error The requested module './helper/interaction.js' does not provide an export named 'animationScroll

--- site.js
+++ site.js
@@ -1,11 +1,10 @@
 import {Router} from "./route/router.js";
-import {showMenu, animationScroll} from "./helper/interaction.js"
+import {showMenu} from "./helper/interaction.js"
 
 class Site{
   constructor(){
     new Router();
     showMenu();
-    animationScroll();
   }
 }
 
