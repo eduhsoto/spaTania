d28d1f64a50d43094fc71d5b98d34d61478d05cd
Eduardo Hernández Soto <hseduardo@gmail.com>
2022-11-12 16:31:20 -0600

    element portafolio

---
+++ components/Project.js
@@ -0,0 +1,116 @@
+"use strict"
+export class Project extends HTMLElement{
+    constructor(){
+        super();
+
+        this.attachShadow({mode: "open"});
+    }
+
+    setTitle(val){
+        return this.setAttribute('title', val);
+    }
+
+    setParagraph(val){
+        return this.setAttribute('paragraph', val);
+    }
+
+    setCategory(val){
+        return this.setAttribute('category', val);
+    }
+
+    setImage(val){
+        return this.setAttribute('img', val);
+    }
+
+    static get observedAttributes() {
+        return [''];
+    }
+    
+    attributeChangedCallback(name, oldVal, newVal) {
+    }
+
+    connectedCallback(){
+        this.shadowRoot.innerHTML = `${this.render()} ${this.styles()}`;
+
+        this.getImage();
+    }
+
+    getImage(){
+        this.$image = this.shadowRoot.querySelector('.img');
+        this.$image.src = this.getAttribute('img');
+    }
+
+    disconnectedCallback(){
+    }
+
+    render(){
+        return `
+        <div class="card__portafolio">
+            <a href="https://www.behance.net/gallery/148786781/WorkClick" target="_blank">
+            <div class="flex">
+                
+                <img alt="portafolio image" class="img image">
+       
+                <div class="detail">
+                    <h1 class="title">${this.getAttribute('title')}</h1>
+                    <p class="category">${this.getAttribute('category')}</p>
+                    <p class="paragraph">${this.getAttribute('paragraph')}</p>
+                </div>
+                </div>
+            </a>
+        </div>
+        `
+    }
+
+    styles(){
+        return `
+        <style>
+
+        :host{
+            --space_elements: 30px 0px;
+        }
+
+        .card__portafolio{
+
+        }
+
+        .image{
+            position: relative;
+        }
+
+        .img{
+
+        }
+
+        .detail{
+            position: absolute;
+            background: rgba(248, 248, 248, 0.8);
+            width: 490px;
+            padding: 20px 30px;
+        }
+
+        .flex{
+            display: flex;
+            align-items: flex-end;
+        }
+
+        h1{
+            font-weight: 800;
+            font-size: 2em;
+            line-height: 49px;
+            color:black;
+            margin-bottom: 20px;
+        }
+
+        p{
+            font-weight: 400;
+            font-size: 1em;
+            color:black;
+            line-height: 24px;
+        }
+        </style>
+        `
+    }
+}
+
+customElements.define('project-element', Project);
--- pages/portafolio.js
+++ pages/portafolio.js
@@ -1,4 +1,6 @@
 import {Head} from "../components/Head.js";
+import {Project} from "../components/Project.js";
+
 "use strict"
 
 export class Portafolio extends HTMLElement{
@@ -8,11 +10,12 @@ export class Portafolio extends HTMLElement{
   
   connectedCallback(){
     this.innerHTML = this.render();
-    this.$globalSection = this.querySelector(".head");
     this.generateHeadElement();
+    this.generatePortafolioElement();
   }
   
   generateHeadElement(){
+    this.$globalSection = this.querySelector(".head");
     let headElement = new Head();
     this.attributesHead(headElement);
     this.$globalSection.appendChild(headElement);
@@ -24,11 +27,29 @@ export class Portafolio extends HTMLElement{
     component.setImage("./assets/img/experience.jpg");
     component.headReverse = false;
   }
-  
+
+  generatePortafolioElement(){
+    this.$globalSection = this.querySelector("#portafolio");
+    let projectElement = new Project();
+    this.attributesProject(projectElement);
+    this.$globalSection.appendChild(projectElement);
+  }
+
+  attributesProject(element){
+    element.setImage("./assets/img/portafolio-item.jpg");
+    element.setTitle("Worclick");
+    element.setCategory("Diseño UX");
+    element.setParagraph("Diseño de sitio web y móvil para el reclutamiento de recursos humanos y búsqueda de empleo.");
+  }
+
   render(){
     return `
     <section class="head">
     </section>
+    <section> 
+      <div id="portafolio" class="container">
+      </div>
+    </section> 
     `
   }
 }
